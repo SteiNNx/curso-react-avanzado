6f80564c0c8dff06824a0790cb691cbc32a07838
Jorge Reyes Galvez <jorge.reyes@sea.gob.cl>
2019-07-23 21:35:24 -0400

    ADD: Now deployement

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,11 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel.loader',
+                    loader: 'babel-loader',
                     options: {
                         presets: [
                             '@babel/preset-env',
-                            '@babel/preset-react',
+                            '@babel/preset-react'
                         ]
                     }
                 }
