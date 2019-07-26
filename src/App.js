f1bf58228363787013f556c4317b785696deac8a
Jorge Reyes Galvez <jorge.reyes@sea.gob.cl>
2019-07-25 22:56:51 -0400

    AVANCES: List y logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from 'react'
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
+import Logo from './components/Logo';
 
-import { GlobalStyle } from './GlobalStyles';
+import { GlobalStyle } from './styles/GlobalStyles';
 
 const App = () => {
     return (
         <div>
             <GlobalStyle />
+            <Logo />
             <ListOfCategories />
             <ListOfPhotoCards />
         </div>
